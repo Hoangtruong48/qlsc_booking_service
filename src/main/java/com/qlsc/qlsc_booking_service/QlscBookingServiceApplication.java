dcf2ac5eec01dc1f4baa168ca321091abc000645
htruong48 <darkai6789@gmail.com>
2025-08-20 17:02:52 +0700

    init project

---
+++ src/main/java/com/qlsc/qlsc_booking_service/QlscBookingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.qlsc.qlsc_booking_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QlscBookingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QlscBookingServiceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/qlsc/qlsc_booking_service/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.qlsc.qlsc_booking_service.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Booking Service API")
+                        .description("API documentation for Booking Service")
+                        .version("1.0"));
+    }
+}
